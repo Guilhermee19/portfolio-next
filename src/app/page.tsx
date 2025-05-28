99a9f67016df7b1f6e694cd7282964f1224db20c
Guilhermee19 <guilherme.santana1998@gmail.com>
2025-05-28 20:58:04 -0300

    feat: add AboutMe and Banner components, implement smooth scrolling
    - Introduced AboutMe component for personal introduction.
    - Created Banner component with a background image and text overlay.
    - Implemented SmoothScrollProvider for enhanced scrolling experience using GSAP.
    - Updated layout to include SmoothScrollProvider for all children components.
    - Removed old Banner component and adjusted imports accordingly.
    - Added custom scrollbar styles in scroll.scss.
    - Updated package.json to include GSAP and text shadow plugin for Tailwind CSS.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,11 @@
-"use client";
-
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls, Stats } from "@react-three/drei";
-import { Suspense } from "react";
-import Noclaf_511 from "./(public)/_components/noclaf-511";
+import AboutMe from "./(public)/_components/about-me";
+import Banner from "./(public)/_components/banner";
 
 export default function Home() {
   return (
-    <div style={{ height: "100vh", width: "100vw", position: "relative" }}>
-      <Canvas shadows camera={{ fov: 40, position: [0, 4, 0] }} dpr={1}>
-        <Suspense fallback={null}>
-          {/* Luzes */}
-          <ambientLight intensity={0.3} />
-          <directionalLight
-            position={[10, 10, 5]}
-            castShadow
-            intensity={1}
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-          />
-
-          {/* Sala 511 */}
-          <Noclaf_511 position={{ x: 0, y: -1, z: 0 }} />
-
-          {/* Controles da câmera (sem movimentar abaixo do piso) */}
-          <LimitCamera />
-          <Stats className="absolute z-20 top-0 left-0" />
-        </Suspense>
-      </Canvas>
-    </div>
+    <>
+      <Banner />
+      <AboutMe />
+    </>
   );
-}
-
-// Trava a câmera para não atravessar o chão
-function LimitCamera() {
-  // const { camera } = useThree();
-
-  // useFrame(() => {
-  //   // Impede que a câmera vá abaixo do chão
-  //   if (camera.position.y < 1) {
-  //     camera.position.y = 1;
-  //   }
-  // });
-
-  return <OrbitControls minDistance={5} maxDistance={50} />;
 }
