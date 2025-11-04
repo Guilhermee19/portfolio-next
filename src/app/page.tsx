1e6bbdb3ce10148bab0f4461928958045ce415cd
Guilhermee19 <guilherme@noclaftech.com>
2025-11-03 22:18:30 -0300

    feat: add Particles component for animated background effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import AboutMe from "./(public)/_components/about-me";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 import Banner from "./(public)/_components/banner";
+import { Particles } from "@/components/ui/particles";
 // import Contact from "./(public)/_components/contact";
 // import Projects from "./(public)/_components/projects";
 
@@ -9,10 +10,14 @@ export default function Home() {
     <>
       <AnimatedThemeToggler
         duration={400}
-        className="fixed z-40 top-2 right-2 dark:text-white"
+        className="fixed z-40 top-4 right-4 dark:text-white"
       />
 
-      <Banner />
+      <Particles className=" fixed w-full h-dvh top-0 z-0" />
+
+      <div className="relative z-10">
+        <Banner />
+      </div>
       {/* <AboutMe /> */}
       {/* <Projects /> */}
       {/* <Contact /> */}
